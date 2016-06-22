728a9fab33fd263e661e9272fcae07b738c6c79f
Andrew Fry <andyfry01@gmail.com>
2016-06-22 12:01:48 -0400

    in the middle of palindrome, not quite working yet

---
+++ number_palindrome.js
@@ -0,0 +1,20 @@
+// Figure out if a given number is a palindrome. You may not
+// convert the number into a string.
+
+var palindrome = 11211
+var notPalindrome = 11221
+
+
+function findPalindrome (number) {
+}
+
+function reverseNum (number) {
+  var reversed = 0;
+  while (number !== 0) {
+    reversed = reversed * 10 + number % 10
+    console.log("reversed after multiplying by 10 and adding number % 10");
+    number /= 10;
+    console.log("number /= 10");
+  }
+  console.log(reversed);
+}
