a5f73840a9cb2a788d47a5ca88e14b8e926254d3
Andrew Fry <andyfry01@gmail.com>
2016-06-30 10:14:26 -0400

    nodes finder fxn completed

--- singly_linked.js
+++ singly_linked.js
@@ -5,7 +5,7 @@
     this.next = undefined
   };
 
-  function SinglyList(node) {
+  function SinglyList() {
     this.length = 0;
     this.head = null;
   }
@@ -19,17 +19,54 @@
       this.head = node;
       this.length += 1;
 
-      return node;
+      console.log(node);
     }
 
-    //adds node to end of list if list is not empty
+    //adds node to end of list if the list is not empty
     while (currentNode.next) {
       currentNode = currentNode.next;
     }
 
     currentNode.next = node;
-
     this.length += 1
-
-    return node;
+    console.log(node);
   }
+
+
+function nodeFinder(list, n) {
+
+  var listLen = list.length;
+  var numSteps = listLen - n;
+  var starter = "list.head"
+  var step = ".next"
+  var steps = step
+
+  var stepFinder = function(numSteps) {
+    for (var i = 0; i < numSteps - 1; i += 1) {
+      steps = steps += step
+    }
+  }
+
+  if (numSteps == 0) {
+    return console.log("The path to the node " + n + " step(s) from the end of the list is:", list.head);
+  } else if (numSteps > 0) {
+
+    stepFinder(numSteps)
+    return console.log("The path to the node " + n + " step(s) from the end of the list is:", starter += steps)
+
+  }
+}
+
+// Test data
+
+var list = new SinglyList()
+
+var nodeOne = new Node("First node")
+var nodeTwo = new Node("Second node")
+var nodeThree = new Node("Third node")
+
+list.add(nodeOne)
+list.add(nodeTwo)
+list.add(nodeThree)
+
+nodeFinder(list, 2)
