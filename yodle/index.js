7ee7131fbbdc1fa7f45a6ced9e1aa925669c8450
Andrew Fry <andyfry01@gmail.com>
2016-07-21 11:23:34 -0400

    triangle calc fxn written and working

--- yodle/index.js
+++ yodle/index.js
@@ -15,18 +15,15 @@ fs.readFile(txtFile, function(err, data){
   var triangleText = data.toString()
 
 
-  var triangleCalculator = {
+  var triangleParser = {
 
     // Stores the max sum for the triangle
     grandTotal: 0,
     triangleArray: [],
 
-    // Reads triangle.txt so we can work with it in the program
-
     // Locates new lines to differentiate between triangle rows
     findNewLine: function (text) {
       if (/\n+/g.test(text)) {
-        console.log("newline found")
         return true
       }
       return false
@@ -35,54 +32,92 @@ fs.readFile(txtFile, function(err, data){
     // Locates spaces to distinguish between individual numbers
     findSpace: function (text) {
       if (/\s+/.test(text)) {
-        console.log("space found")
         return true
       }
       return false
     },
 
+    // Locates numbers
     findNum: function (text) {
       if (/\d+/.test(text)) {
-        console.log("number found")
         return true
       }
       return false
     },
 
+    // Builds a hash table of triangle rows from the txt file
     buildTriangleArray: function (triangle) {
-      console.log(triangle);
       var triangleLength = triangle.length
-      console.log(triangleLength);
 
       var number = ""
+      var numIndex = 0
       var rowArray = []
 
       for (var i = 0; i < triangleLength; i++) {
-        console.log(triangle[i]);
-        // Each triangle row has two newline characters at the end, hence findNewLine is called for i and i+1
         if (this.findNewLine(triangle[i])) {
             this.triangleArray.push(rowArray)
             rowArray = []
-            i += 2
+            numIndex = 0
+            i += 1
         }
         if (this.findNum(triangle[i])) {
           number += triangle[i]
         }
         if (this.findSpace(triangle[i])) {
           if (number != "") {
-            rowArray.push(number)
+            rowArray.push({number: parseInt(number), index: numIndex})
+            numIndex += 1
             number = ""
           }
         }
       }
-      console.log(this.triangleArray);
-      // fs.writeFile('array.txt', triangleCalculator.triangleArray, function(error){
-      //   console.log(error);
-      // });
+      triangleCalculator.doMath(this.triangleArray)
     }
-
   }
 
-  triangleCalculator.buildTriangleArray(triangleText)
 
+  var triangleCalculator = {
+
+    // Checks index of nodes in triangle row for doMath fxn
+    checkIndex: function(triangleNode) {
+      if (triangleNode - 1 === undefined) {
+        return true
+      }
+      return false
+    },
+
+    // Performs calculation on triangle array to find max sum
+    doMath: function(triangleArray) {
+      var arrayLength = triangleArray.length
+      var runningTotal = 0
+
+      var startNum = triangleArray[0][0]
+
+      var currentNode = startNum
+      var firstNum = undefined
+      var secondNum = undefined
+
+      runningTotal += currentNode.number
+
+      for (var i = 1; i < arrayLength; i++) {
+
+        console.log("the current node is", currentNode);
+
+        firstNum = triangleArray[i][currentNode.index]
+        secondNum = triangleArray[i][currentNode.index + 1]
+
+        console.log("firstNum:", firstNum, "secondNum", secondNum);
+
+        if (firstNum.number > secondNum.number) {
+          runningTotal += firstNum.number
+          currentNode = firstNum
+        } else if (firstNum.number < secondNum.number) {
+          runningTotal += secondNum.number
+          currentNode = secondNum
+        }
+      }
+      console.log(runningTotal)
+    }
+  }
+  triangleParser.buildTriangleArray(triangleText)
 })
