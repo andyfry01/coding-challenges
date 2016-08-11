b0e303b59492a1d7bef8ce7660353c9157096e2b
Andy Fry <andyfry01@gmail.com>
2016-08-11 11:47:32 -0400

    hex converter can now account for negative numbers

--- hexadecimal_converter.js
+++ hexadecimal_converter.js
@@ -2,6 +2,7 @@ const hexConverter = {
 
   data: {
     hexArray: [],
+    isNegative: false,
     binary: undefined
   },
 
@@ -18,21 +19,35 @@ const hexConverter = {
 
     typeof(hex) === "number" ? console.log("Error, you must pass in a hexadecimal value") : null
     if (typeof(hex) === "string") {
+      this.data.hexArray = []
       for (val in hex) {
-        this.data.hexArray.push(hex[val])
+        if (hex[val] === '-') {
+          this.data.hexArray.push(hex[val])
+        } else if (!parseInt(hex[val])) {
+          this.data.hexArray.push(hex[val].toUpperCase())
+        } else if (parseInt(hex[val])) {
+          this.data.hexArray.push(hex[val])
+        }
       }
     }
   },
 
   convertHex: function(hex) {
 
-    let hexMultiplier = hex.length - 1
+    let hexMultiplier = undefined
+
+    hex[0] === '-' ? hexMultiplier = hex.length - 2 : hexMultiplier = hex.length - 1
+
     let hexConversion = 0
+
+    // Reset data for new conversion
+    this.data.binary = undefined
+    this.data.isNegative = false
 
     for (val in hex) {
 
       // First checks if the digit is an integer or a string.
-      // Need to parse the strigified integer before we can perform the calculations
+      // Need to parse the strigified integer before we can perform the conversion
       if (parseInt(hex[val])) {
 
         let num = parseInt(hex[val])
@@ -47,22 +62,32 @@ const hexConverter = {
       }
 
       // If the digit is a letter, we then compare it to the list of hex letter values
-      // in the hexLetterVals object and then do the calculations on that value
+      // in the hexLetterVals object and then do the conversion on that value
       if (!parseInt(hex[val])) {
 
         let letter = hex[val]
-        let num = this.hexLetterVals[letter]
 
-        if (hexMultiplier === 0) {
-          hexConversion += num
+        // Checks to see if the input number is negative
+        if (letter === '-') {
+
+          this.data.isNegative = true
+
+        // If it's not, perform conversion
+        } else if (letter !== '-') {
+
+          let num = this.hexLetterVals[letter]
+          if (hexMultiplier === 0) {
+            hexConversion += num
+          }
+          if (hexMultiplier > 0) {
+            hexConversion += (num * Math.pow(16, hexMultiplier))
+            hexMultiplier -= 1
+          }
         }
-        if (hexMultiplier > 0) {
-          hexConversion += (num * Math.pow(16, hexMultiplier))
-          hexMultiplier -= 1
-        }
+
       }
     }
-    this.data.binary = hexConversion
+    this.data.isNegative === true ? this.data.binary = hexConversion * -1 : this.data.binary = hexConversion
     console.log(`The hexadecimal number ${hex.join('')} in base 2 is ${this.data.binary}`);
   }
 
