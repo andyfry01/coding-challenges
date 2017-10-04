e140dddc1664ad71f985742f43d92c75db91b32d
Andy Fry <andyfry01@gmail.com>
2017-10-04 13:38:00 -0400

    assembling packets, checking for packet completeness and printing completed packets

--- packet_assembler/app.js
+++ packet_assembler/app.js
@@ -13,54 +13,119 @@ const packetIndexExpression = /(?:^\d{4}\s+)(\d+)(?:\s+)/
 const packetLengthExpression = /(?:^\d{4}\s+\d+\s+)(\d+)(\s{0,})/
 const packetDataExpression = /(?:^\d{4}\s+\d+\s+\d+)(.{0,})/
 
-let packetData = {}
+let packetDataObject = {}
+let deliveredPackets = []
+
+let lineDivider = ''
+let numDividerCharacters = process.stdout.columns
+for (let i = 0; i < numDividerCharacters; i++) {
+  lineDivider += '-'
+}
+
 
 function sendData(data){
+  return new Promise((resolve, reject) => {
+    let updatedPacket = updatePacket(data)
+    if (updatedPacket) {
+      resolve(updatedPacket)
+    } else {
+      reject({error: data})
+    }
+  })
+}
 
+function updatePacket(packetData){
+  let updatedPacket = undefined
+  // build data object by parsing packet for relevant parts
   let dataObj = {
-    messageID: data.match(messageIDExpression)[0],
-    packetIndex: parseInt(data.match(packetIndexExpression)[1], 10),
-    packetLength: parseInt(data.match(packetLengthExpression)[1], 10),
-    packetData: data.match(packetDataExpression)[1]
+    messageID: packetData.match(messageIDExpression)[0],
+    packetIndex: parseInt(packetData.match(packetIndexExpression)[1], 10),
+    packetLength: parseInt(packetData.match(packetLengthExpression)[1], 10),
+    packetData: packetData.match(packetDataExpression)[1]
   }
 
-  let packetDataUpdate = Object.assign({}, packetData)
-
+  let packetDataUpdate = Object.assign({}, packetDataObject)
+  // If we haven't seen the messageID in the packet stream already, create a new entry to track it in the packetDataObject, with an empty array matching the indicated length of the incoming data
   if (packetDataUpdate[dataObj.messageID] === undefined) {
     let emptyArray = Array(dataObj.packetLength).fill(0)
-    packetDataUpdate = Object.assign({}, packetData, {[dataObj.messageID]: emptyArray})
-    packetData = packetDataUpdate
+    packetDataUpdate = Object.assign({}, packetDataUpdate, {[dataObj.messageID]: emptyArray})
+    updatedPacket = packetDataUpdate[dataObj.messageID]
   }
-  packetDataUpdate[dataObj.messageID] = packetDataUpdate[dataObj.messageID].map((item, index) => {
+  // map through the packetDataUpdate array and add the incoming packet at the correct index in the array
+  updatedPacket = packetDataUpdate[dataObj.messageID].map((item, index) => {
     if (dataObj.packetIndex === index) {
       return dataObj
     } else {
       return item
     }
   })
-  packetData = packetDataUpdate
+  let packetObject = {}
+  packetObject.messageID = dataObj.messageID
+  packetObject.packetArray = updatedPacket
+  return packetObject
+}
+
+function testForCompletePacket(packetData) {
+  // test if any index in the assembled packet hasn't been filled in with the corresponding line from the packet. If there are gaps, we haven't received the whole packet yet.
+	let result = undefined
+	packetData.forEach((item, index) => {
+    if (item === 0) {
+      result = false
+    }
+    if (index === packetData.length - 1 && result === undefined) {
+      result = true
+    }
+	})
+	return result
+}
+
+function checkDeliveryStatus(messageID) {
+  // Check to see if this particular packet has been delievered already. If it has been delievered, do not print it (seeing as we already have), if it has not been delivered, add messageID to array for tracking.
+  if (deliveredPackets.indexOf(messageID) > -1) {
+    return true
+  } else {
+    deliveredPackets.push(messageID)
+    return false
+  }
+}
+
+function printAssembledPacket(packetData) {
+  // check for delivery status, assemble and print package
+  if (checkDeliveryStatus(packetData.messageID) === true) {
+    return
+  } else {
+    let output = ''
+    packetData.packetArray.forEach(line => {
+      output += line.packetData
+    })
+    console.log('')
+    console.log('')
+    console.log(lineDivider);
+    console.log(`messageID: ${packetData.messageID}`);
+    console.log(`message content:`)
+    console.log(output);
+    console.log(lineDivider);
+    console.log('')
+    console.log('')
+  }
 }
 
 testFiles.forEach((file) => {
+  // loop through test files array, deliver 'packets' from test files at random intervals to be parsed, assembled, tested and ultimately printed.
   let lineReader = require('readline').createInterface({
     input: require('fs').createReadStream(file)
   });
 
   lineReader.on('line', function (line) {
+    // sets random 'delivery' interval so packets in an arbitrary order at arbitrary times
     let randNumMiliseconds = (Math.random() * 5 + 1) * 1000
-    sendData(line)
-
-    // setTimeout(() => {
-    // }, randNumMiliseconds)
-    //
+    setTimeout(() => {
+      sendData(line).then(updatedPacketData => {
+        packetDataObject[updatedPacketData.messageID] = updatedPacketData.packetArray
+        if (testForCompletePacket(updatedPacketData.packetArray) === true) {
+          printAssembledPacket(updatedPacketData)
+        }
+      })
+    }, randNumMiliseconds)
   });
-  lineReader.on('close', () => {
-    console.log('packetData is:');
-    console.log(packetData);
-  })
-
 })
-
-
-// let test = Map({hi: 'andy', thisIs: 'an immutable map'})
-// console.log(test);
--- packet_assembler/readme.md
+++ packet_assembler/readme.md
@@ -3,7 +3,3 @@
 When a message is transmitted over the internet, it is split into multiple packets, each packet is transferred individually, and the packets are reassembled into the original message by the receiver. Because the internet exists in the real world, and because the real world can be messy, packets do not always arrive in the order in which they are sent. For today's challenge, your program must collect packets from stdin, assemble them in the correct order, and print the completed messages to stdout.
 
 The point of reading from stdin is to simulate incoming packets. For the purposes of this challenge, assume there is a potentially unlimited number of packets. Your program should not depend on knowing how many packets there are in total. Simply sorting the input in its entirety would technically work, but defeats the purpose of this exercise.
-
-## Approach for solving problem:
-
-1. Simulate incoming packets arriving out of order and timing by sending packets at random intervals after reading them with fs.
