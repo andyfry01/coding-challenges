9a71ccb2cd5591bf72f7e44955aa352516c47aca
Andy Fry <andyfry01@gmail.com>
2017-10-03 20:38:56 -0400

    added content to test files, reading test files in app.js

--- packet_assembler/app.js
+++ packet_assembler/app.js
@@ -1,1 +1,24 @@
 const fs = require('fs')
+const path = require('path')
+console.log('starting up file');
+
+const testFiles = [
+  path.resolve(__dirname, 'test_input_txts', 'testInput1.txt'),
+  path.resolve(__dirname, 'test_input_txts', 'testInput2.txt'),
+  path.resolve(__dirname, 'test_input_txts', 'testInput3.txt')
+]
+
+// console.log('testFiles are');
+// console.log(testFiles);
+testFiles.forEach(testFile => {
+  var readStream = fs.createReadStream(testFile, {encoding: 'utf8'});
+
+  readStream.on('data', function (chunk) {
+    console.log(chunk);
+  })
+
+  readStream.on('end', function () {  // done
+    console.log(`finished reading ${testFile}`);
+  });
+
+})
