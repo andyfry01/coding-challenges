336eb7c0caf164508096d0e0cb3de16b4f76f435
Andy Fry <andyfry01@gmail.com>
2017-10-03 23:21:03 -0400

    can now  parse data and update packetData object after reading each line

--- packet_assembler/app.js
+++ packet_assembler/app.js
@@ -1,4 +1,5 @@
 const path = require('path')
+
 console.log('starting up file');
 
 const testFiles = [
@@ -6,11 +7,38 @@ const testFiles = [
   path.resolve(__dirname, 'test_input_txts', 'testInput2.txt'),
   path.resolve(__dirname, 'test_input_txts', 'testInput3.txt')
 ]
-//
-function sendData(data, randNumMiliseconds){
-  console.log('data is');
-  console.log(data);
-  console.log(`sent after ${randNumMiliseconds} miliseconds`);
+
+const messageIDExpression = /\d{4}/
+const packetIndexExpression = /(?:^\d{4}\s+)(\d+)(?:\s+)/
+const packetLengthExpression = /(?:^\d{4}\s+\d+\s+)(\d+)(\s{0,})/
+const packetDataExpression = /(?:^\d{4}\s+\d+\s+\d+)(.{0,})/
+
+let packetData = {}
+
+function sendData(data){
+
+  let dataObj = {
+    messageID: data.match(messageIDExpression)[0],
+    packetIndex: parseInt(data.match(packetIndexExpression)[1], 10),
+    packetLength: parseInt(data.match(packetLengthExpression)[1], 10),
+    packetData: data.match(packetDataExpression)[1]
+  }
+
+  let packetDataUpdate = Object.assign({}, packetData)
+
+  if (packetDataUpdate[dataObj.messageID] === undefined) {
+    let emptyArray = Array(dataObj.packetLength).fill(0)
+    packetDataUpdate = Object.assign({}, packetData, {[dataObj.messageID]: emptyArray})
+    packetData = packetDataUpdate
+  }
+  packetDataUpdate[dataObj.messageID] = packetDataUpdate[dataObj.messageID].map((item, index) => {
+    if (dataObj.packetIndex === index) {
+      return dataObj
+    } else {
+      return item
+    }
+  })
+  packetData = packetDataUpdate
 }
 
 testFiles.forEach((file) => {
@@ -20,9 +48,19 @@ testFiles.forEach((file) => {
 
   lineReader.on('line', function (line) {
     let randNumMiliseconds = (Math.random() * 5 + 1) * 1000
-    setTimeout(() => {
-      sendData(line, randNumMiliseconds)
-    }, randNumMiliseconds)
+    sendData(line)
+
+    // setTimeout(() => {
+    // }, randNumMiliseconds)
+    //
   });
+  lineReader.on('close', () => {
+    console.log('packetData is:');
+    console.log(packetData);
+  })
 
 })
+
+
+// let test = Map({hi: 'andy', thisIs: 'an immutable map'})
+// console.log(test);
